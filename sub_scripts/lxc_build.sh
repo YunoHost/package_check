c4aac9feb9229b4fe72a32909aa3da3f8746a399
Maniack Crudelis <maniackc_dev@crudelis.fr>
2017-08-06 01:07:12 +0200

    Force dns par défaut

--- sub_scripts/lxc_build.sh
+++ sub_scripts/lxc_build.sh
@@ -23,7 +23,7 @@ test -n "$DOMAIN" || DOMAIN=domain.tld
 test -n "$YUNO_PWD" || YUNO_PWD=admin
 test -n "$LXC_NAME" || LXC_NAME=pchecker_lxc
 test -n "$LXC_BRIDGE" || LXC_BRIDGE=lxc-pchecker
-test -n "$dnsforce" || dnsforce=0
+test -n "$dnsforce" || dnsforce=1
 ARG_SSH="-t"
 
 # Tente de définir l'interface réseau principale
