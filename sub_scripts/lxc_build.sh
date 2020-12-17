c7336ca0ad46186ce545c5aa3bf3910d3effb90a
Jens Diemer <github.com@jensdiemer.de>
2020-12-17 08:39:27 +0100

    Update lxc_build.sh
    On a fresh installed debian system is `lsb-release` not installed.

--- sub_scripts/lxc_build.sh
+++ sub_scripts/lxc_build.sh
@@ -80,7 +80,7 @@ echo -e "# Branche à utiliser pour le script d'install de cette distribution (s
 
 echo -e "\e[1m> Update et install lxc lxctl\e[0m" | tee -a "$LOG_BUILD_LXC"
 sudo apt-get update >> "$LOG_BUILD_LXC" 2>&1
-sudo apt-get install -y lxc lxctl >> "$LOG_BUILD_LXC" 2>&1
+sudo apt-get install -y lsb-release lxc lxctl >> "$LOG_BUILD_LXC" 2>&1
 
 echo -e "\e[1m> Install git, curl and lynx\e[0m" | tee -a "$LOG_BUILD_LXC"
 sudo apt-get install -y git curl lynx jq python3-pip >> "$LOG_BUILD_LXC" 2>&1
