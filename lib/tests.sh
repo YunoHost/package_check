22d703cb4805d1e7037f6189c11624d00bc2c2de
Alexandre Aubin <alex.aubin@mailoo.org>
2024-09-01 17:55:26 +0200

    bash syntax check: gotta return 0 for the job to be flagged as failure

--- lib/tests.sh
+++ lib/tests.sh
@@ -32,7 +32,9 @@ _STUFF_TO_RUN_BEFORE_INITIAL_SNAPSHOT() {
         log_report_test_success
     else
         echo "{'level': 0}" >$result_json
-        log_critical "Obvious syntax issues found which will make the scripts crash ... not running the actual tests until these are fixed"
+        log_error "Obvious syntax issues found which will make the scripts crash ... not running the actual tests until these are fixed"
+        # Return 0 instead of 1 such that the job gets flagged as failure and not as error
+        return 0
     fi
 
     # We filter apt deps starting with $app_id to prevent stupid issues with for example cockpit and transmission where the apt package is not properly reinstalled on reinstall-after-remove test ...
